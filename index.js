219eeac62f839e835f7e31d1a7cebddbe76777a8
ychilal87-netizen <ychilal87@gmail.com>
2026-05-08 22:20:20 +0300

    Update Firebase configuration and fix process signal handling

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const serviceAccount = {
   "project_id": "evpapp-354e3",
   "private_key_id": "a14d48b8130418cc50a60895ab02ae9bc9069f46",
   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDX5ycR+ltPRXeN\nBy8PkJM2rpT+vk7YdLQPMrl0BGkCE0LzXiCLGzP4ANqmRaomXBNOH96QmOmSEwje\n3VutJzip0eVBtMnMae5BRyFg9SNcCmXMRnwSrV2Q/yOAX36MGXpkbCWjq/rkVCr4\nB6EuX2JB2GC8MirgV6Ll591hyD/UoASUHRqHDQJO9UFHsJD/H9RK2KqNB6GstlmG\n/J8N11YGoWebVd9sWNKg80TlBQMXji/lElnaKiDXrK+Pac9GjIW4f73oBf47FPQq\n9Hj6aBLf568uPVuMMcgfHulA4VBl3joQ8OpQEGpgw9yLnxpImkNIJ3NOfm4GGC4B\njIc/tQtzAgMBAAECggEABrmaVvrZ8/5DgmmPknkK4abbewrusKfQnTHG/JOC/bLs\nfoNobm3hhfS16+LyfgJkJDd2pWNiv6kHH6xrQXMu8BawEf3o/x2Vc6Y2hHO7GdzO\nP86PcazSsKZ+HV2kpdKVvfLV5F7f6dwRKzy1miFG74jacsm1puxoKbkxOFxFR2Lr\n8T5/gvLMOk31tEV4BkmXlNUfrlVXhonAJdAF/QYEcEQ00bejgIOFOZAVsmZ4MPhN\n1oGC+GdTau2AlxMVbpqhaqFfXIS9QW1K4aTgHXVae8JV/rvP6WwVW9V3naHUHryn\naDsloIMGK93GuS9ylpKeWHWqBIiHtSpKhc5KnF2cRQKBgQD4VO0eauQDwA0oMLK2\BzxnyQilqGxcOljihdJwwsM/KH1nz8/aYtveXXRF/mnw5eJrbXc/C7i4+XMtJaKo\vu0+tqEdpd79acpr/Rhutw/QIuGY5ULEKO9xzsOjPVHK/gZWc4K5wE3NRQTCwJfd\n1zIIdlDXjvf7nNQXvwAlnRJVRwKBgQDekeANYphu7ABaylagcDjM1BdHAzPohfTf\nq0LPfAg7DKki6gTrgZMCHWJJc9WD/bL01+65b1omCR+j25VItFnUO2t+7TO+WA4R\ngtPfd08FeMEgtLoaD5OAn69rQ4h6sjMs1DfAf+TjqtMlaRb0/guPh7WiMtJiMl8k\njFxU/txedQKBgQCg9AKAHY3N9HNSokWif4wiRIVrQX+CQ3sKzuu3lDSSQf8OAv5Y\MI0LUg0jtKMbtRQMxvVsEn/WvC9vip4juyPv6tUAb1rZavD9Dxz/3XzNTV5lB+s/\nUrznSbk5m2vhIsYe0F1hYYfLzLkqNUP5UBooq+73171ZAqvg/DXvKyAPzwKBgQCS\EOYborMrSJLLArNrQ3rjYeWM4xsz5IXaLEkCZgH4zsxJb4+fB0B1SNdyISTlJKBH\nc1bmN5/QCnjLTpZgSowRbpWTjRRCPQOulk95/pDLpH4KetEPvV0uMp4rJPzymPRg\n3T1hhg2vJDLCAH/idL8diRC4y+WqMCikkcEevsvc6QKBgQCev7A1R5yFwV4Kf7je\n6kUgi9PMvwWZDfbkRN1CNzFvuWsBG0Y4KB36jtDMiZpKqU4S/0ikjXwhzEBv6nkO\nY75mSKVo24Ok7Y0sJMdrKM+zX6ZL5I5NMMhgs71ByBKtYntUMu/Fm6VDw5ryPsC7\nrw/uzrGm0XyAcG/C7s6Peq8hsg==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@evpapp-354e3.iam.gserviceaccount.com",
-  "client_id": "111786763110985458360",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40evpapp-354e3.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+  "client_email": "firebase-adminsdk-fbsvc@evpapp-354e3.iam.gserviceaccount.com"
 };
 
 try {
